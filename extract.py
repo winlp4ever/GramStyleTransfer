f1fb8ef5e235e28ff75e870ee51ff7f451b7c61a
Le Ha Quang <winlp4ever@gmail.com>
2018-09-24 00:38:32 +0200

    update preprocessing and postprocessing

--- extract.py
+++ extract.py
@@ -1,10 +1,12 @@
 import torch
 import torchvision
+from torchvision import transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.model_zoo as model_zoo
 import numpy as np
+from PIL import Image
 
 import cv2
 
@@ -34,20 +36,46 @@ class vgg_extractor(VGG):
             indx = indx_
         return chosen_features
 
+def preprocess(fn, im_size=224):
+    im = Image.open(fn)
+    if isinstance(im_size, int):
+        height = width = im_size
+    elif isinstance(im_size, (tuple, list)) and len(im_size) == 2:
+        height = im_size[0]
+        width = im_size[1]
+    else:
+        raise ValueError('not an accepted dim!')
+    prep = transforms.Compose([transforms.Resize((height, width)),
+                               transforms.ToTensor(),
+                               transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),  # turn to BGR
+                               transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961],  # subtract imagenet mean
+                                                    std=[1, 1, 1]),
+                               transforms.Lambda(lambda x: x.mul_(255)),
+                               ])
+    return prep(im)
 
-def crop_image(fn, new_width=224, new_height=224):
-    im = cv2.imread(fn, cv2.IMREAD_UNCHANGED)
-    dim = (new_width, new_height)
-    return cv2.resize(im, dim, interpolation=cv2.INTER_AREA)
+
+def postprocess(tensor):
+    postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1. / 255)),
+                                 transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961],  # add imagenet mean
+                                                      std=[1, 1, 1]),
+                                 transforms.Lambda(lambda x: x[torch.LongTensor([2, 1, 0])]),  # turn to RGB
+                                 ])
+    postpb = transforms.Compose([transforms.ToPILImage()])
+    t = postpa(tensor)
+    t[t>1]=1
+    t[t<0]=0
+    img = postpb(t)
+    return img
 
 
 def vgg19_bn_extractor(**kwargs):
     kwargs['init_weights'] = False
 
-    model = vgg_extractor(make_layers(cfg['E'], batch_norm=True), **kwargs)
+    model = vgg_extractor(make_layers(cfg['E'], batch_norm=False), **kwargs)
     model_dict = model.state_dict()
 
-    pretrained_dict = model_zoo.load_url(model_urls['vgg19_bn'])
+    pretrained_dict = model_zoo.load_url(model_urls['vgg19'])
     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
 
     model_dict.update(pretrained_dict)
@@ -64,13 +92,21 @@ def mseloss(input, target):
     return loss
 
 
-def synthesize(model, device, im, epochs, lr):
+def synthesize(model, device, im, epochs, lr, im_size=224):
     for param in model.parameters():
         param.requires_grad = False
 
-    x = torch.zeros(3, 224, 224, requires_grad=True, device='cuda')
+    if isinstance(im_size, int):
+        height = width = im_size
+    elif isinstance(im_size, (tuple, list)) and len(im_size) == 2:
+        height = im_size[0]
+        width = im_size[1]
+    else:
+        raise ValueError('not an accepted dim!')
 
-    feats = model.forward(torch.from_numpy(im).float().to(device))
+    x = torch.zeros(3, height, width, requires_grad=True, device=device)
+
+    feats = model.forward(im.float().to(device))
     feats = [u.detach() for u in feats]
 
     #optimizer = optim.SGD([x], lr=lr, momentum=momentum)
@@ -86,27 +122,30 @@ def synthesize(model, device, im, epochs, lr):
         l = optimizer.step(closure)
 
         print('epoch:{}--loss:{}'.format(epoch, l), end='\r', flush=True)
-        if epoch % 100 == 0:
-            resynth_img = np.transpose(x.cpu().detach().numpy(), (1, 2, 0))
-            cv2.imshow('synthesized img', resynth_img * 255)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+        if epoch % 10 == 0:
+            out_t = x.cpu()
+            out_img = postprocess(out_t)
+            print(type(out_img))
+            out_img.show()
 
 
 if __name__ == '__main__':
-    device = torch.device("cuda")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     E = vgg19_bn_extractor()
+    #print(E.features)
     E = E.to(device)
-    ls_layers = (2, 9, 16, 29, 42)
+    #ls_layers = (2, 9, 16, 29, 42)
+    #ls_layers = (0, 7, 14, 27, 40)
+    ls_layers = (1, 6, 11, 20, 29)
     E.set_ls_layers(ls_layers)
     filename = './images/im.jpg'
-    resized_img = crop_image(filename)
-    resized_img = np.transpose(resized_img, (2, 0, 1))
-    resized_img = resized_img / 255
-    print('shape {}'.format(resized_img.shape))
-    print(resized_img)
+    resized_img = preprocess(filename, im_size=(224, 512))
+    print(resized_img.shape)
 
-    synthesize(E, device, resized_img, 100, 0.8)
+    print('shape {}'.format(resized_img.shape))
+    #print(resized_img)
+
+    synthesize(E, device, resized_img, 100, 0.8, (224, 512))
 
 
 
