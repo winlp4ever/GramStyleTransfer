dc4b9e76355243a00faf3b014aab7d612b1cb96c
Le Ha Quang <winlp4ever@gmail.com>
2018-09-24 14:32:03 +0200

    add style loss of gram matrices into the func

--- extract.py
+++ extract.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 import torch.utils.model_zoo as model_zoo
 import numpy as np
 from PIL import Image
-
+import argparse
 import cv2
 
 from model import VGG
@@ -36,6 +36,7 @@ class vgg_extractor(VGG):
             indx = indx_
         return chosen_features
 
+
 def preprocess(fn, im_size=224):
     im = Image.open(fn)
     if isinstance(im_size, int):
@@ -63,8 +64,8 @@ def postprocess(tensor):
                                  ])
     postpb = transforms.Compose([transforms.ToPILImage()])
     t = postpa(tensor)
-    t[t>1]=1
-    t[t<0]=0
+    t[t > 1] = 1
+    t[t < 0] = 0
     img = postpb(t)
     return img
 
@@ -84,15 +85,26 @@ def vgg19_bn_extractor(**kwargs):
     return model
 
 
-def mseloss(input, target):
+def mseloss(input, target, size_average=True):
     loss = 0
     for u, v in zip(input, target):
-        loss += F.mse_loss(u, v)
+        loss += F.mse_loss(u, v, size_average)
 
     return loss
 
 
-def synthesize(model, device, im, epochs, lr, im_size=224):
+def style_loss(features):
+    ls_grams = []
+    for f in features:
+        f_ = f.view(f.shape[1], -1)
+        n = f_.shape[0]
+        m = f_.shape[1]
+        gram_f = torch.matmul(f_, torch.transpose(f_, 0, 1)) / (4 * n ** 2 * m ** 2)
+        ls_grams.append(gram_f)
+    return ls_grams
+
+
+def synthesize(model, device, im_c, im_s, epochs, lr, im_size=224, lambd=1.0):
     for param in model.parameters():
         param.requires_grad = False
 
@@ -106,46 +118,63 @@ def synthesize(model, device, im, epochs, lr, im_size=224):
 
     x = torch.zeros(3, height, width, requires_grad=True, device=device)
 
-    feats = model.forward(im.float().to(device))
-    feats = [u.detach() for u in feats]
+    # content features
+    feats_cnt = model.forward(im_c.float().to(device))
+    feats_cnt = [u.detach() for u in feats_cnt]
 
-    #optimizer = optim.SGD([x], lr=lr, momentum=momentum)
+    # style features
+    feats_stl = model.forward(im_s.float().to(device))
+    grams = style_loss(feats_stl)
+    grams = [g.detach() for g in grams]
+
+    # optimizer = optim.SGD([x], lr=lr, momentum=momentum)
     optimizer = optim.LBFGS([x], max_iter=100, lr=lr)
     print('set up finished!')
     for epoch in range(1, epochs + 1):
         def closure():
             optimizer.zero_grad()
             output = model.forward(x)
-            loss = mseloss(output, feats)
+            out_grams = style_loss(output)
+            l_c = mseloss(output, feats_cnt)
+            l_s = lambd * mseloss(out_grams, grams, size_average=False)
+            loss = l_c + l_s
+            print('epoch:{0}/{1}--loss: total {2:.2f} content {3:.2f} style {4:.2f} '.
+                  format(epoch, epochs, loss, l_c, l_s), end='\r', flush=True)
             loss.backward()
             return loss
-        l = optimizer.step(closure)
 
-        print('epoch:{}--loss:{}'.format(epoch, l), end='\r', flush=True)
+        optimizer.step(closure)
+
         if epoch % 10 == 0:
             out_t = x.cpu()
             out_img = postprocess(out_t)
-            print(type(out_img))
             out_img.show()
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--size', type=int, nargs='*', default=224)
+    parser.add_argument('--lambd', '-l', type=float, default=1e10)
+
+    args = parser.parse_args()
+    size = args.size
+    if len(size) < 2:
+        size = size[0]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     E = vgg19_bn_extractor()
-    #print(E.features)
+    # print(E.features)
     E = E.to(device)
-    #ls_layers = (2, 9, 16, 29, 42)
-    #ls_layers = (0, 7, 14, 27, 40)
+    # ls_layers = (2, 9, 16, 29, 42)
+    # ls_layers = (0, 7, 14, 27, 40)
     ls_layers = (1, 6, 11, 20, 29)
     E.set_ls_layers(ls_layers)
-    filename = './images/im.jpg'
-    resized_img = preprocess(filename, im_size=(224, 512))
-    print(resized_img.shape)
-
-    print('shape {}'.format(resized_img.shape))
-    #print(resized_img)
-
-    synthesize(E, device, resized_img, 100, 0.8, (224, 512))
+    fn_c = './images/cnt.jpg'
+    img_c = preprocess(fn_c, im_size=size)
+    print('shape {}'.format(img_c.shape))
+    # print(resized_img)
+    fn_s = './images/stl.jpg'
+    img_s = preprocess(fn_s, im_size=size)
 
 
-
+    synthesize(E, device, img_c, img_s, args.epochs, 0.8, size, lambd=args.lambd)
